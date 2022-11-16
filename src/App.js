5e297d9029530193d35c6efaa6ef11abe9df7358
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2022-11-16 22:24:42 +0100

    changed api

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
 
   useEffect(() => {
     const getRecipe = async () => {
-      const response = await fetch(`https://api.edamam.com/search?q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
       const data = await response.json();
+      console.log(data);
       console.log(data.hits);
       setMyRecipes(data.hits);
     }
---
+++ src/ProductPage.js
@@ -0,0 +1,9 @@
+function ProductPage() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ProductPage;
